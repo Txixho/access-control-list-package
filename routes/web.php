2c16072c7d93d1d0f2ced09d059e5f8da996b43d
Jairo Alvite <jairo.alvite@fba-consulting.com>
2023-11-10 13:58:25 +0100

    correcion nombre controladores por cambio para mayor claridad

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Fbaconsulting\Aclpackage2\Http\Controllers\AdministracionClientesController;
+use Fbaconsulting\Aclpackage2\Http\Controllers\AdministracionPerfilesClientesController;
 use Fbaconsulting\Aclpackage2\Http\Controllers\AdministracionPerfilesController;
-use Fbaconsulting\Aclpackage2\Http\Controllers\AdministracionUsuariosController;
+use Fbaconsulting\Aclpackage2\Http\Controllers\AdministracionPerfilesUsuariosController;
 use Fbaconsulting\Aclpackage2\Http\Controllers\PerfilController;
 use Fbaconsulting\Aclpackage2\Http\Controllers\PerfilRutaController;
 use Fbaconsulting\Aclpackage2\Http\Controllers\TablaRutasController;
@@ -31,18 +31,18 @@ Route::middleware(['ruta'])->group(function () {
 
     Route::put('/admin/perfil-rutas/{perfil}', [PerfilRutaController::class, 'update'])->name('perfil-rutas.update');
 
-    Route::get('/admin/administracion-usuarios', [AdministracionUsuariosController::class, 'index'])->name('admin.administracion-usuarios.index');
+    Route::get('/admin/administracion-usuarios', [AdministracionPerfilesUsuariosController::class, 'index'])->name('admin.administracion-usuarios.index');
 
-    Route::post('/admin/administracion-usuarios/update', [AdministracionUsuariosController::class, 'Update'])
+    Route::post('/admin/administracion-usuarios/update', [AdministracionPerfilesUsuariosController::class, 'Update'])
         ->name('actualizar-perfiles-usuarios');
 
     Route::get('/listado-rutas', [TablaRutasController::class, 'index'])->name('listado-rutas');
 
     Route::post('/almacenar-rutas', [TablaRutasController::class, 'storeRutas'])->name('almacenar-rutas');
 
-    Route::get('/admin/administracion-clientes',  [AdministracionClientesController::class, 'index'])->name('admin.clientes.index');
+    Route::get('/admin/administracion-clientes',  [AdministracionPerfilesClientesController::class, 'index'])->name('admin.clientes.index');
 
-    Route::post('/admin/administracion-clientes/update', [AdministracionClientesController::class, 'update'])
+    Route::post('/admin/administracion-clientes/update', [AdministracionPerfilesClientesController::class, 'update'])
         ->name('actualizar-perfiles-clientes');
 
     Route::get('/admin/administracion-perfiles', [AdministracionPerfilesController::class, 'index'])->name('administracion-perfiles.index');
