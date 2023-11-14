0edc110b9067ff0f23bfd8a61042e437c798f412
Jairo Alvite <jairo.alvite@fba-consulting.com>
2023-11-14 09:31:05 +0100

    agregado metodo al trait

--- src/Traits/UsuarioTrait.php
+++ src/Traits/UsuarioTrait.php
@@ -3,6 +3,7 @@
 namespace Fbaconsulting\Aclpackage2\Traits;
 
 use Fbaconsulting\Aclpackage2\Models\PerfilClientesUsuario;
+use Fbaconsulting\Aclpackage2\Models\PerfilCliente;
 
 
 trait UsuarioTrait
@@ -11,4 +12,10 @@ trait UsuarioTrait
     {
         return $this->hasMany(PerfilClientesUsuario::class, 'usuario_id', 'usuario_id');
     }
+
+    public function perfilCliente()
+    {
+        return $this->hasOne(PerfilCliente::class, 'usuario_id', 'usuario_id');
+    }
+
 }
