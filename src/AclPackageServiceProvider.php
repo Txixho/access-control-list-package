9f682595ae23de0a329575d0ec2e018b06181157
Jairo Alvite <jairo.alvite@fba-consulting.com>
2023-11-14 11:03:53 +0100

    agregado metodo para registrar y publicar las vistas

--- src/AclPackageServiceProvider.php
+++ src/AclPackageServiceProvider.php
@@ -17,11 +17,19 @@ class AclPackageServiceProvider extends ServiceProvider
     {
         // Obtener el router a través del contenedor de servicios de Laravel
         $router = $this->app['router'];
+
         // Registrar middlewares
         $router->aliasMiddleware('comprobar.usuario.ruta', ComprobarRutaUsuario::class);
-
         $router->aliasMiddleware('comprobar.ruta', ComprobarAccesoRuta::class);
 
-    // Aquí también puedes registrar rutas, vistas, y publicar archivos de configuración.
+        //Registrar vistas
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'aclpackage2');
+        $this->loadViewsFrom(__DIR__.'/../resources/views/admin', 'aclpackage2-admin');
+
+        // Permitir la publicación de las vistas
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/aclpackage2'),
+            __DIR__.'/../resources/views/admin' => resource_path('views/vendor/aclpackage2-admin'),
+        ], 'aclpackage2-views');
     }
 }
