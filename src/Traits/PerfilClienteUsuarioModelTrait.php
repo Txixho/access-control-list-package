985cbcc7ec61ea3414f73696318f5eaa92a72c50
Jairo Alvite <jairo.alvite@fba-consulting.com>
2023-11-15 13:15:49 +0100

    creadoPerfilClienteUsuarioModelTrait

---
+++ src/Traits/PerfilClienteUsuarioModelTrait.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace FbaConsulting\AccessControlListPackage\Traits;
+
+trait PerfilClienteUsuarioModelTrait
+{
+    protected $perfilClienteModel = \App\Model\Cliente::class;
+    protected $usuarioModel = \App\Model\Usuario::class;
+
+    protected $perfilClienteForeignKey = 'perfil_cliente_id';
+    protected $usuarioForeignKey = 'usuario_id';
+
+    protected $localKey = 'perfil_cliente_usuario_id';
+
+    // Métodos para cambiar los modelos
+    public function setPerfilClienteModel($model)
+    {
+        $this->perfilClienteModel = $model;
+    }
+
+    public function setUsuarioModel($model)
+    {
+        $this->usuarioModel = $model;
+    }
+
+    // Métodos para configurar las claves foráneas y la clave local
+    public function setPerfilClienteForeignKey($foreignKey)
+    {
+        $this->perfilClienteForeignKey = $foreignKey;
+    }
+
+    public function setUsuarioForeignKey($foreignKey)
+    {
+        $this->usuarioForeignKey = $foreignKey;
+    }
+
+    public function setLocalKey($localKey)
+    {
+        $this->localKey = $localKey;
+    }
+
+    // Métodos para obtener relaciones
+    public function getObenerPerfilCliente()
+    {
+        return $this->belongsTo($this->perfilClienteModel, $this->perfilClienteForeignKey, $this->localKey);
+    }
+
+    public function getObtenerUsuario()
+    {
+        return $this->belongsTo($this->usuarioModel, $this->usuarioForeignKey, $this->localKey);
+    }
+}
