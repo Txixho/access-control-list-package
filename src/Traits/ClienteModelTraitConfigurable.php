5ee575769b9f89d0d30790fa0ed5ca59650ab014
Jairo Alvite <jairo.alvite@fba-consulting.com>
2023-11-15 14:20:15 +0100

    creado trait ClienteModelTraitConfigurable usando el ConfigurableRelationsTrait

---
+++ src/Traits/ClienteModelTraitConfigurable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FbaConsulting\AccessControlListPackage\Traits;
+
+trait ClienteModelTraitConfigurable
+{
+    use ConfigurableRelationsTrait;
+
+    public function getObtenerPerfiles()
+    {
+        return $this->belongsToMany($this->relatedModels['perfil'] ?? \App\Model\Perfil::class,
+            $this->pivotTables['clientePerfil'] ?? 'perfiles_clientes',
+            $this->localKeys['cliente'] ?? 'cliente_id',
+            $this->foreignKeys['perfil'] ?? 'perfil_id')
+            ->withPivot($this->pivotExtraColumns['clientePerfil'] ?? ['nombre_personalizado']);
+    }
+}
